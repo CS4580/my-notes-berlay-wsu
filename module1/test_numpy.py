e2d3c35153a8d919b5430774f5aefdeed49627ac
berlay-wsu <mikhailberlay@mail.weber.edu>
2024-09-04 10:13:22 -0600

    my notes

---
+++ module1/test_numpy.py
@@ -0,0 +1,16 @@
+#make sure the python environment is cs4580
+
+"""First look at Numpy
+"""
+import numpy as np # is missing in the environment
+
+def main():
+    """
+        Driven Function
+    """
+    arr_1d = np.array([0,1,2])
+    print(arr_1d, type(arr_1d))
+    
+
+if __name__ == '__main__':
+    main()
