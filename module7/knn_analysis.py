28d1a0a856580acabb736d6ddf5268ec7aff079d
berlay-wsu <mikhailberlay@mail.weber.edu>
2024-11-04 09:28:52 -0700

    knn euclidean distance

--- module7/knn_analysis.py
+++ module7/knn_analysis.py
@@ -2,56 +2,64 @@
 """
 import pandas as pd
 import numpy as np
-import get_data as gt  # you package
-
+import get_data as gt  # your package
 
 # Constants
 K = 10  # number of closest matches
-BASE_CASE_ID = 88763  # first id in the dataset. IMDB_id for 'Back to the future'
-
-
-def distance(p, q):
-    pass
+BASE_CASE_ID = 88763  # IMDB_id for 'Back to the Future'
 
 
 def metric_stub(base_case_value, comparator_value):
     return 0
 
 
-def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sort_values='metric'):
-    df = data_df.copy()
-    # WIP: Create df of filtered data
-    # first class citizen in Python
-    df[sort_values] = df[comparison_type].map(
+def print_top_k(df, sorted_value, comparison_type):
+    print(f'Top {K} closest matches by {comparison_type}')
+    counter = 1
+    for idx, row in df.head(K).iterrows():
+        print(f"""Top {counter} match: [{idx}]:{row['year']}
+        {row['title']}, {row['genres']}, {row[sorted_value]}]""")
+        counter += 1
+
+
+def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
+    df = data_df.copy()  # make a copy of the dataframe
+    # WIP: Create df of filter data
+    df[sorted_value] = df[comparison_type].map(
         lambda x: metric_func(base_case[comparison_type], x))
     # Sort return values from function stub
-    sorted_df = df.sort_values(by=sort_values)
-    sorted_df.drop(BASE_CASE_ID, inplace=True)  # drop the base case
-    print(sorted_df['title'].head(K))  # print top ten values
+    sorted_df = df.sort_values(by=sorted_value)
+    sorted_df.drop(BASE_CASE_ID, inplace=True)  # drop base case
+    # print(sorted_df['title'].head(K))  # print top ten values
+    print_top_k(sorted_df, sorted_value, comparison_type)
+
+
+def euclidean_distance(base_case_year: int, comparator_year: int):
+    return abs(base_case_year - comparator_year)
 
 
 def main():
     # TASK 1: Get dataset from server
-    print(f'Task 1: Download dataset from server')
+    print(f'\nTask 1: Download dataset from server')
     dataset_file = 'movies.csv'
     gt.download_dataset(gt.ICARUS_CS4580_DATASET_URL, dataset_file)
-
     # TASK 2: Load  data_file into a DataFrame
-    print(f'Task 2: Load movie data into a DataFrame')
+    print(f'\nTask 2: Load movie data into a DataFrame')
     data_file = f'{gt.DATA_FOLDER}/{dataset_file}'
     data = gt.load_data(data_file, index_col='IMDB_id')
     print(f'Loaded {len(data)} records')
-    print(f'Data set columns {data.columns}')
-    print(f'Dataset description {data.describe()}')
-
-    # Task 3 : KNN simple analysis
-    print(f'Task 3: KNN simple analysis')
+    print(f'Data set Columns {data.columns}')
+    print(f'Data set description {data.describe()}')
+    # Task 3: KNN Analysis Driver
+    print(f'\nTask 3:KNN Simple Analysis')
     base_case = data.loc[BASE_CASE_ID]
-    print(f"Comparing all movies to our base case: {base_case['title']}")
-    knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
-                        metric_func=metric_stub, sort_values='metric')
-
-    # TODO: The rest of your code goes here
+    print(f"Comparing all movies to our case: {base_case['title']}")
+    knn_analysis_driver(data_df=data, base_case=base_case,
+                        comparison_type='genres', metric_func=metric_stub, sorted_value='metric')
+    # Task 4: Euclidean distance based on Year
+    print(f'\nTask 4: Euclidean distance based on Year')
+    knn_analysis_driver(data_df=data, base_case=base_case,
+                        comparison_type='year', metric_func=euclidean_distance, sorted_value='euclidean_distance')
 
 
 if __name__ == '__main__':
